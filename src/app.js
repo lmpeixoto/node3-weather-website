60d0ad29c9fe6f1a8e2b7723eb5d9b527640cda4
Luis Peixoto <lmpeixoto@gmail.com>
2020-01-31 21:57:27 +0000

    footer header partials

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 console.log(__dirname)
 console.log(path.join(__dirname, '../public'))
@@ -8,12 +9,13 @@ const app = express()
 
 //  Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
-
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -35,6 +37,7 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help',
+        name: 'Andrew Mead',
         message: 'This page serves the purpose of helping you with your questions.'
     })
 })
