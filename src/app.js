6638e82ed95e048673382bcd55a4a4ea6720dcf9
Luis Peixoto <lmpeixoto@gmail.com>
2020-01-31 22:12:56 +0000

    partials before challenge

--- src/app.js
+++ src/app.js
@@ -49,6 +49,14 @@ app.get('/weather', (req, res) => {
     })
 })
 
+app.get('/help/*', (req, res) => {
+    res.send('Help article not found')
+})
+
+app.get('*', (req, res) => {
+    res.send('My 404 page')
+})
+
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
 })
