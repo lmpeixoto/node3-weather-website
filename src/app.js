3454b53f7abd19a80b28f384ec1dbed17aab29eb
Luis Peixoto <lmpeixoto@gmail.com>
2020-02-01 20:42:34 +0000

    before copying

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const forecast = require('./utils/forecast')
+const geocode = require('./utils/geocode')
 
 console.log(__dirname)
 console.log(path.join(__dirname, '../public'))
@@ -42,19 +44,55 @@ app.get('/help', (req, res) => {
     })
 })
 
-app.get('/weather', (req, res) => {
+aapp.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+
+
+app.get('/products', (req, res) => {
+    console.log(req.query)
     res.send({
-        forecast: 'The temperature is 32ºC',
-        location: 'Lisbon, Portugal'
+        products: []
     })
 })
 
 app.get('/help/*', (req, res) => {
-    res.send('Help article not found')
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Help article not found!',
+        name: 'Andrew Mead'
+    })
 })
 
 app.get('*', (req, res) => {
-    res.send('My 404 page')
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Page not found!',
+        name: 'Andrew Mead'
+    })
 })
 
 app.listen(3000, () => {
