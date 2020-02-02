6d3551503143d2cac0b7921a790637e1af37878c
Luis Peixoto <lmpeixoto@gmail.com>
2020-02-02 08:32:12 +0000

    before git and heroku

--- Public/js/app.js
+++ Public/js/app.js
@@ -5,6 +5,10 @@ console.log('Client side javascript file is loaded!')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+// messageOne.textContent = 'From Javascript'
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -13,21 +17,25 @@ weatherForm.addEventListener('submit', (e) => {
 
     const url = 'http://localhost:3000/weather?address=' + location
 
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+    
     fetch(url).then((response) => {
-    response.json().then((data) => {
+        response.json().then((data) => {
 
-        if (data.error) {
-            console.log(data.error)
-        } else {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
 
-            console.log(data.location)
-            console.log(data.forecast)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+                
 
 
-        } 
-        
+            }
+
+        })
     })
-})
 
 
 })
